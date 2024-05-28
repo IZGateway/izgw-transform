1ad11635c85ff58b27793f02ea769d62d2c10e6a
Austin Moody <austin.moody@gmail.com>
2024-05-28 15:31:08 -0400

    Refactor PipelineBuilder.
    The refactoring changes the process of building a pipeline in the PipelineBuilder class. The sequence of operations has been rearranged and the creation of a new Hl7Pipeline object is now only done when necessary.

--- src/main/java/gov/cdc/izgateway/transformation/pipelines/PipelineBuilder.java
+++ src/main/java/gov/cdc/izgateway/transformation/pipelines/PipelineBuilder.java
@@ -23,42 +23,41 @@ public class PipelineBuilder {
     }
 
     public Hl7Pipeline build(ServiceContext context) throws Exception {
-        Hl7Pipeline pipeline = new Hl7Pipeline();
-
         // Get pipeline configuration via the context
         // Context has Organzation, Inbound Endpoint & Outbound Endpoint
         // Should only be 1 Pipeline for that combination of information
         PipelineConfig pipelineConfig = serviceConfig.getPipelineConfig(context);
 
-        // build chain
-        if (pipelineConfig != null) {
-            // TODO - make generic isn't necessarily always going to be an Hl7Pipeline we are building here.
-            pipeline = new Hl7Pipeline(pipelineConfig);
+        if (pipelineConfig == null) {
+            return new Hl7Pipeline();
+        }
 
-            // A pipeline will have "pipes"
-            for (PipeConfig pipeConfig : pipelineConfig.getPipes()) {
-                Hl7v2Pipe pipe = new Hl7v2Pipe();
+        // TODO - make generic isn't necessarily always going to be an Hl7Pipeline we are building here.
+        Hl7Pipeline pipeline = new Hl7Pipeline(pipelineConfig);
 
-                for (Precondition precondition : pipeConfig.getPreconditions()) {
-                    if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(Equals.class)) {
-                        pipe.addPrecondition(new Hl7v2Equals((Equals) precondition));
-                    } else if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(NotEquals.class)) {
-                        pipe.addPrecondition(new Hl7v2NotEquals((NotEquals) precondition));
-                    }
+        // A pipeline will have "pipes"
+        for (PipeConfig pipeConfig : pipelineConfig.getPipes()) {
+            Hl7v2Pipe pipe = new Hl7v2Pipe();
+
+            for (Precondition precondition : pipeConfig.getPreconditions()) {
+                if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(Equals.class)) {
+                    pipe.addPrecondition(new Hl7v2Equals((Equals) precondition));
+                } else if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(NotEquals.class)) {
+                    pipe.addPrecondition(new Hl7v2NotEquals((NotEquals) precondition));
                 }
-
-                // Get Solution configuration from full system configuration
-                Optional<SolutionConfig> solutionConfig = serviceConfig.getSolutionConfigById(pipeConfig.getSolutionId());
-
-                if (solutionConfig.isPresent()) {
-                    // TODO - need to pass context here to get DataType or pass DataType.  Need to clean this up.
-                    pipe.setSolution(new Solution(solutionConfig.get(), context.getDataType()));
-                } else {
-                    throw new Exception(String.format("Solution not found in system with ID %s", pipeConfig.getSolutionId()));
-                }
-
-                pipeline.addPipe(pipe);
             }
+
+            // Get Solution configuration from full system configuration
+            Optional<SolutionConfig> solutionConfig = serviceConfig.getSolutionConfigById(pipeConfig.getSolutionId());
+
+            if (solutionConfig.isPresent()) {
+                // TODO - need to pass context here to get DataType or pass DataType.  Need to clean this up.
+                pipe.setSolution(new Solution(solutionConfig.get(), context.getDataType()));
+            } else {
+                throw new Exception(String.format("Solution not found in system with ID %s", pipeConfig.getSolutionId()));
+            }
+
+            pipeline.addPipe(pipe);
         }
 
         return pipeline;
