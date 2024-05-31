435c700db7b18356db465c36af131e32654374c5
Austin Moody <austin.moody@gmail.com>
2024-05-30 21:51:51 -0400

    Refactor pipeline and pipe construction in transformation process
    Revised the approach of building pipeline and constructing pipes within the transformation process. Added precondition handling logic in BasePipe constructor to eliminate redundancy in PipelineBuilder. Adjusted HL7v2Pipe to support the new BasePipe constructor.

--- src/main/java/gov/cdc/izgateway/transformation/pipes/Hl7v2Pipe.java
+++ src/main/java/gov/cdc/izgateway/transformation/pipes/Hl7v2Pipe.java
@@ -1,12 +1,14 @@
 package gov.cdc.izgateway.transformation.pipes;
 
+import gov.cdc.izgateway.transformation.configuration.PipeConfig;
+import gov.cdc.izgateway.transformation.context.ServiceContext;
 import lombok.extern.java.Log;
 
 @Log
 public class Hl7v2Pipe extends BasePipe implements Pipe {
 
-    public Hl7v2Pipe() {
-        super();
+    public Hl7v2Pipe(PipeConfig pipeConfig, ServiceContext context) {
+        super(pipeConfig, context);
     }
 
 }
