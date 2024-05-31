72016f8b484c61a4c178b28e27bf2770ead1a5e7
Austin Moody <austin.moody@gmail.com>
2024-05-30 21:51:51 -0400

    Refactor pipeline and pipe construction in transformation process
    Revised the approach of building pipeline and constructing pipes within the transformation process. Added precondition handling logic in BasePipe constructor to eliminate redundancy in PipelineBuilder. Adjusted HL7v2Pipe to support the new BasePipe constructor.

--- src/main/java/gov/cdc/izgateway/transformation/pipelines/PipelineBuilder.java
+++ src/main/java/gov/cdc/izgateway/transformation/pipelines/PipelineBuilder.java
@@ -1,10 +1,8 @@
 package gov.cdc.izgateway.transformation.pipelines;
 
-import gov.cdc.izgateway.transformation.enums.DataType;
 import gov.cdc.izgateway.transformation.configuration.*;
 import gov.cdc.izgateway.transformation.context.ServiceContext;
 import gov.cdc.izgateway.transformation.pipes.Hl7v2Pipe;
-import gov.cdc.izgateway.transformation.preconditions.*;
 import gov.cdc.izgateway.transformation.solutions.Solution;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,39 +21,25 @@ public class PipelineBuilder {
     }
 
     public Hl7Pipeline build(ServiceContext context) throws Exception {
-        // Get pipeline configuration via the context
-        // Context has Organzation, Inbound Endpoint & Outbound Endpoint
-        // Should only be 1 Pipeline for that combination of information
+        // Get pipeline configuration via the context.
+        // Context has Organization, Inbound Endpoint & Outbound Endpoint.
+        // Should only be 1 Pipeline for that combination of information.
         PipelineConfig pipelineConfig = serviceConfig.getPipelineConfig(context);
 
         if (pipelineConfig == null) {
             return new Hl7Pipeline();
         }
 
-        // TODO - make generic isn't necessarily always going to be an Hl7Pipeline we are building here.
         Hl7Pipeline pipeline = new Hl7Pipeline(pipelineConfig);
 
         // A pipeline will have "pipes"
         for (PipeConfig pipeConfig : pipelineConfig.getPipes()) {
-            Hl7v2Pipe pipe = new Hl7v2Pipe();
-
-            for (Precondition precondition : pipeConfig.getPreconditions()) {
-                if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(Equals.class)) {
-                    pipe.addPrecondition(new Hl7v2Equals((Equals) precondition));
-                } else if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(NotEquals.class)) {
-                    pipe.addPrecondition(new Hl7v2NotEquals((NotEquals) precondition));
-                } else if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(Exists.class)) {
-                    pipe.addPrecondition(new Hl7v2Exists((Exists) precondition));
-                } else if (context.getDataType().equals(DataType.HL7V2) && precondition.getClass().equals(NotExists.class)) {
-                    pipe.addPrecondition(new Hl7v2NotExists((NotExists) precondition));
-                }
-            }
+            Hl7v2Pipe pipe = new Hl7v2Pipe(pipeConfig, context);
 
             // Get Solution configuration from full system configuration
             Optional<SolutionConfig> solutionConfig = serviceConfig.getSolutionConfigById(pipeConfig.getSolutionId());
 
             if (solutionConfig.isPresent()) {
-                // TODO - need to pass context here to get DataType or pass DataType.  Need to clean this up.
                 pipe.setSolution(new Solution(solutionConfig.get(), context.getDataType()));
             } else {
                 throw new Exception(String.format("Solution not found in system with ID %s", pipeConfig.getSolutionId()));
