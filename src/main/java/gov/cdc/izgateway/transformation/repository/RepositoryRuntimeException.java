e8fc14e60c83ab973a7eef338171c80058e3ab2a
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-06-10 12:51:36 -0400

    Adding organization service and repository code.

---
+++ src/main/java/gov/cdc/izgateway/transformation/repository/RepositoryRuntimeException.java
@@ -0,0 +1,7 @@
+package gov.cdc.izgateway.transformation.repository;
+
+public class RepositoryRuntimeException extends RuntimeException {
+    public RepositoryRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
