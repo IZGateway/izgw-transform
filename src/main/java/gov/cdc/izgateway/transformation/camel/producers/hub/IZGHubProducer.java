53ae79616dc9761cbe1497c60b2b46a9e4b2fdb6
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-06-18 13:09:34 -0400

    Latest updates and some changes from code review.

--- src/main/java/gov/cdc/izgateway/transformation/camel/producers/hub/IZGHubProducer.java
+++ src/main/java/gov/cdc/izgateway/transformation/camel/producers/hub/IZGHubProducer.java
@@ -2,13 +2,9 @@ package gov.cdc.izgateway.transformation.camel.producers.hub;
 
 import ca.uhn.hl7v2.HL7Exception;
 import gov.cdc.izgateway.model.IDestination;
-import gov.cdc.izgateway.model.IDestinationId;
-import gov.cdc.izgateway.soap.fault.UnknownDestinationFault;
 import gov.cdc.izgateway.soap.message.SubmitSingleMessageResponse;
 import gov.cdc.izgateway.transformation.context.HubWsdlTransformationContext;
 import gov.cdc.izgateway.transformation.endpoints.hub.HubControllerFault;
-import gov.cdc.izgateway.transformation.endpoints.hub.forreview.Destination;
-import gov.cdc.izgateway.transformation.endpoints.hub.forreview.DestinationId;
 import gov.cdc.izgateway.transformation.endpoints.hub.forreview.HubMessageSender;
 import gov.cdc.izgateway.transformation.enums.DataFlowDirection;
 import gov.cdc.izgateway.transformation.util.Hl7Utils;
@@ -18,7 +14,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.support.DefaultProducer;
 
 @Slf4j
-public class IZGHubProducer  extends DefaultProducer {
+public class IZGHubProducer extends DefaultProducer {
 
     public IZGHubProducer(Endpoint endpoint) {
         super(endpoint);
@@ -26,10 +22,8 @@ public class IZGHubProducer  extends DefaultProducer {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        log.info("IZGHubProducer");
         IZGHubComponent hubComponent = (IZGHubComponent) getEndpoint().getComponent();
         HubMessageSender messageSender = hubComponent.getMessageSender();
-        log.info("IZGHubProducer 2");
 
         HubWsdlTransformationContext context = exchange.getIn().getBody(HubWsdlTransformationContext.class);
         try {
@@ -38,8 +32,9 @@ public class IZGHubProducer  extends DefaultProducer {
         catch (HL7Exception e) {
             throw new HubControllerFault(e.getMessage());
         }
+
         // TODO: Paul - discussed with Keith and this destination will be a fixed thing - not a destination IIS... think about this more.
-        IDestination dest = getDestination("0");
+        IDestination dest = hubComponent.getDestination("0");
 
         SubmitSingleMessageResponse response = messageSender.sendSubmitSingleMessage(dest, context.getSubmitSingleMessageRequest());
         context.getServiceContext().setCurrentDirection(DataFlowDirection.RESPONSE);
@@ -48,15 +43,4 @@ public class IZGHubProducer  extends DefaultProducer {
 
     }
 
-    private IDestination getDestination(String destinationId) throws UnknownDestinationFault {
-        IDestinationId destinationIdObject = new DestinationId();
-        destinationIdObject.setDestId(destinationId);
-        destinationIdObject.setDestType(0);
-
-        IDestination hubDestination = new Destination();
-        hubDestination.setId(destinationIdObject);
-        hubDestination.setDestUri("https://localhost/IISHubService");
-
-        return hubDestination;
-    }
 }
