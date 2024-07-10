7a9bc42317067fb8d52b087f34fce24fa9328977
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-07-10 14:50:49 -0400

    Adding latest code after debugging the XformAdviceAspect.  Join point was running on the method call and the method execute - getting into the aspect twice.  Change was made to the Around annotation to specify just execution and the specific annotation.

---
+++ src/main/java/gov/cdc/izgateway/transformation/logging/advice/Advisable.java
@@ -0,0 +1,5 @@
+package gov.cdc.izgateway.transformation.logging.advice;
+
+public interface Advisable {
+    public String getName();
+}
--- src/main/java/gov/cdc/izgateway/transformation/pipelines/BasePipeline.java
+++ src/main/java/gov/cdc/izgateway/transformation/pipelines/BasePipeline.java
@@ -3,11 +3,12 @@ package gov.cdc.izgateway.transformation.pipelines;
 import ca.uhn.hl7v2.HL7Exception;
 import gov.cdc.izgateway.transformation.configuration.PipelineConfig;
 import gov.cdc.izgateway.transformation.context.ServiceContext;
+import gov.cdc.izgateway.transformation.logging.advice.Advisable;
 
 // TODO - Need to make generic, meaning not using HAPI at this level because
 //        classes that extend this will not all be HL7 related
 
-abstract class BasePipeline implements Pipeline {
+abstract class BasePipeline implements Pipeline, Advisable {
 
     PipelineConfig configuration;
 
@@ -25,6 +26,11 @@ abstract class BasePipeline implements Pipeline {
         if (configuration != null) {
             executeThisPipeline(context);
         }
+    }
+
+    @Override
+    public String getName() {
+        return configuration.getName();
     }
 
     public abstract void executeThisPipeline(ServiceContext context) throws HL7Exception;
