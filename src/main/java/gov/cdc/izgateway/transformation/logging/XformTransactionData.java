669ea5103de0c3cb16b114c3652ec4eb26a6af78
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-07-16 12:44:53 -0400

    Code changes for code cleanup and reduction.

---
+++ src/main/java/gov/cdc/izgateway/transformation/logging/XformTransactionData.java
@@ -0,0 +1,111 @@
+package gov.cdc.izgateway.transformation.logging;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import gov.cdc.izgateway.logging.event.TransactionData;
+import gov.cdc.izgateway.logging.markers.MarkerObjectFieldName;
+import gov.cdc.izgateway.logging.markers.Markers2;
+import gov.cdc.izgateway.transformation.enums.DataFlowDirection;
+import gov.cdc.izgateway.transformation.logging.advice.*;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+@Slf4j
+@Data
+@MarkerObjectFieldName("transactionData")
+@JsonInclude(JsonInclude.Include.ALWAYS)
+public class XformTransactionData extends TransactionData {
+
+    // TODO Add @JsonIgnore as we don't with these to be wrriten to the log!!! PHI!
+    private ArrayList<XformAdviceRecord> xformAdviceList = new ArrayList<>();
+
+    private PipelineAdvice pipelineAdvice = null;
+    private SolutionAdvice currentSolutionAdvice = null;
+
+    public XformTransactionData() {
+        super();
+    }
+
+    public XformTransactionData(String eventId) {
+        super(eventId);
+    }
+
+    // TODO - Paul
+    // next thing to try is to simplify and not use a reducer concept.
+    // change addAdvice to do the logic I'm doing in the reducer'
+    // If I need to keep track of lastSolutionAdvice or currentSolutionAdivce, just get the last item in the list/array
+
+    public void addAdviceOriginal(XformAdviceRecord advice) {
+        xformAdviceList.add(advice);
+        // reduceAdviceList(advice);
+    }
+
+    public void addAdvice(XformAdviceRecord advice) {
+
+        XformAdvice rootTransformAdvice = null;
+        XformAdvice currentTransformAdvice = null;
+
+        if ( AdviceUtil.isPipelineAdvice(advice.className()) ) {
+            if ( advice.dataFlowDirection() == DataFlowDirection.REQUEST ) {
+                if ( advice.methodDisposition() == MethodDisposition.PREEXECUTION ) {
+                    pipelineAdvice = new PipelineAdvice(advice.descriptorId(), advice.className(), advice.descriptor());
+                    pipelineAdvice.setRequest(advice.requestMessage());
+                }
+                else
+                    pipelineAdvice.setTransformedRequest(advice.requestMessage());
+            } else {
+                if ( advice.methodDisposition() == MethodDisposition.PREEXECUTION )
+                    pipelineAdvice.setResponse(advice.responseMessage());
+                else
+                    pipelineAdvice.setTransformedResponse(advice.responseMessage());
+            }
+            return;
+        }
+
+
+        if ( AdviceUtil.isSolutionAdvice(advice.className()) ) {
+            // get the solution by ID
+            currentSolutionAdvice = pipelineAdvice.getSolutionAdvice(advice);
+
+            if ( advice.methodDisposition() == MethodDisposition.POSTEXECUTION && advice.hasTransformed() ) {
+                if ( advice.dataFlowDirection() == DataFlowDirection.REQUEST )
+                    currentSolutionAdvice.setTransformedRequest(advice.requestMessage());
+                else
+                    currentSolutionAdvice.setTransformedResponse(advice.responseMessage());
+            }
+
+            return;
+        }
+
+        if ( advice.className().contains("Operation") && advice.methodDisposition() == MethodDisposition.POSTEXECUTION) {
+            OperationAdvice operationTransformAdvice = new OperationAdvice(advice.className(), advice.descriptor());
+            if ( advice.dataFlowDirection() == DataFlowDirection.REQUEST ) {
+                operationTransformAdvice.setTransformedRequest(advice.requestMessage());
+                currentSolutionAdvice.addRequestOperationAdvice(operationTransformAdvice);
+            }
+            else {
+                operationTransformAdvice.setTransformedResponse(advice.responseMessage());
+                currentSolutionAdvice.addResponseOperationAdvice(operationTransformAdvice);
+            }
+            return;
+        }
+
+    }
+
+
+
+
+    private boolean isTransformed(XformAdviceRecord advice) {
+        if ( advice.className().equals("Solution")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void logIt() {
+        computeTransactionTimes();
+        log.info(Markers2.append("transactionData", this), "{}", getMessage());
+    }
+}
--- src/main/java/gov/cdc/izgateway/transformation/logging/advice/SolutionAdvice.java
+++ src/main/java/gov/cdc/izgateway/transformation/logging/advice/SolutionAdvice.java
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 public class SolutionAdvice extends XformAdvice {
     private final ArrayList<OperationAdvice> requestOperationAdviceList = new ArrayList<>();
     private final ArrayList<OperationAdvice> responseOperationAdviceList = new ArrayList<>();
+    private String id;
 
-    public SolutionAdvice(String className, String name) {
+    public SolutionAdvice(String id, String className, String name) {
         super(className, name);
+        this.id = id;
     }
 
     public void addRequestOperationAdvice(OperationAdvice operationAdvice) {
@@ -20,4 +22,19 @@ public class SolutionAdvice extends XformAdvice {
     public void addResponseOperationAdvice(OperationAdvice operationAdvice) {
         responseOperationAdviceList.add(operationAdvice);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SolutionAdvice other) {
+            return this.getId().equals(other.getId());
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getId().hashCode();
+    }
+
 }
