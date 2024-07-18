3c9a93dcdc49dda0511cf966953bbe4aa233cb33
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-07-18 06:59:54 -0400

    Committing major refactor.  where we simplify XformTransactionData and simplify the Advice objects.

--- src/main/java/gov/cdc/izgateway/transformation/aspects/xformadvice/XformAspectDetail.java
+++ src/main/java/gov/cdc/izgateway/transformation/aspects/xformadvice/XformAspectDetail.java
@@ -1,33 +1,14 @@
 package gov.cdc.izgateway.transformation.aspects.xformadvice;
 
 import gov.cdc.izgateway.transformation.enums.DataFlowDirection;
+import gov.cdc.izgateway.transformation.logging.advice.XformAdvice;
 import lombok.Data;
 
-/**
- * This record represents a transformation advice.  Transformation advice is used to log the state of a transformation
- * as it is being processed.  This data will be used to create header records in the response messages.  The advice
- * shows the consumer the state of the transformation at the time of the advice.
- */
 @Data
-public abstract class XformAspectDetail {
-    private String name;
-    private String className;
-    private String methodName;
-    private MethodDisposition methodDisposition;
-    private String requestMessage;
-    private String responseMessage;
+public class XformAspectDetail extends XformAdvice {
     private DataFlowDirection dataFlowDirection;
 
     public XformAspectDetail() {
     }
 
-    public XformAspectDetail(String name, String className, String methodName, MethodDisposition methodDisposition, String requestMessage, String responseMessage, DataFlowDirection dataFlowDirection) {
-        this.name = name;
-        this.className = className;
-        this.methodName = methodName;
-        this.methodDisposition = methodDisposition;
-        this.requestMessage = requestMessage;
-        this.responseMessage = responseMessage;
-        this.dataFlowDirection = dataFlowDirection;
-    }
 }
