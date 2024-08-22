aad42e9509f7ecc4e48e68198525d89553c33ea0
pcahillai <61790005+pcahillai@users.noreply.github.com>
2024-08-22 09:39:29 -0400

    Igdd 1679 adding build number api (#52)
    * Adding code to provide an API to return the build number.
    
    * Adding Postman test for build number.
    
    * Adding Postman test for build number.
    
    ---------
    
    Co-authored-by: Paul Cahill <Paul.Cahill@pointclickcare.com>

--- src/main/java/gov/cdc/izgateway/transformation/TSApplicationController.java
+++ src/main/java/gov/cdc/izgateway/transformation/TSApplicationController.java
@@ -1,6 +1,9 @@
 package gov.cdc.izgateway.transformation;
 
 import gov.cdc.izgateway.transformation.services.PipelineRunnerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,4 +24,13 @@ public class TSApplicationController {
         return "Hello from ApplicationController!";
     }
 
+    @Operation(summary = "Get build information about the application",
+            description = "Returns the build number, build machine and build date.")
+    @ApiResponse(responseCode = "200", description = "Success",
+            content = @Content(mediaType = "text/plain")
+    )
+    @GetMapping({"/build", "/build.txt"})
+    public String getBuild() {
+        return Application.getPage(Application.BUILD);
+    }
 }
