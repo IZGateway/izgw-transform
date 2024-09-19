b2f0b2c5877f356ae82fb5ec1d401b538629aced
Paul Cahill <Paul.Cahill@pointclickcare.com>
2024-09-19 06:51:28 -0400

    Latest changes.

--- src/main/java/gov/cdc/izgateway/transformation/camel/routers/TransformationRouter.java
+++ src/main/java/gov/cdc/izgateway/transformation/camel/routers/TransformationRouter.java
@@ -51,6 +51,12 @@ public class TransformationRouter extends RouteBuilder {
         .to("izghub:IISHubService")
         .bean(dataTransformerService);
 
+      from("direct:izgfhir")
+              .bean(fhirToV2Transformer)
+              .bean(dataTransformerService)
+              .to("izghub:IISHubService")
+              .bean(dataTransformerService);
+
 //    from("file:/Users/cahilp/temp/hl7?noop=true")
 //      .to("direct:izghubTransformerPipeline")
 //      .to("file:/Users/cahilp/temp/hl7/processed?fileName=${date:now:yyyyMMddHHmmssSSS}.txt");
