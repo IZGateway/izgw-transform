860112503bc352ec6e80942a6de6f43c0913373c
Austin Moody <amoody@ainq.com>
2025-02-10 15:01:04 -0500

    Remove InvalidPreconditionException.java which isn't being used.

--- src/main/java/gov/cdc/izgateway/xform/exceptions/InvalidPreconditionException.java
+++
@@ -1,7 +0,0 @@
-package gov.cdc.izgateway.xform.exceptions;
-
-public class InvalidPreconditionException extends RuntimeException {
-    public InvalidPreconditionException(String message) {
-        super(message);
-    }
-}
