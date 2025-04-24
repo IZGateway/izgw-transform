882a1f46467fc64d2555a00666ab7b75934153c4
Paul Cahill <pcahill@ainq.com>
2025-04-24 08:46:52 -0400

    Adding logic to handle user-org access controls for pipelines.

--- src/main/java/gov/cdc/izgateway/xform/model/User.java
+++ src/main/java/gov/cdc/izgateway/xform/model/User.java
@@ -3,9 +3,11 @@ package gov.cdc.izgateway.xform.model;
 import gov.cdc.izgateway.xform.validation.ValidOrganization;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -17,9 +19,9 @@ public class User implements BaseModel {
     @NotNull(message = "User - ID is required")
     private UUID id;
 
-    @NotNull(message = "User - Organization is required")
-    @ValidOrganization(message = "Organization ID must reference an existing and active organization")
-    private UUID organizationId;
+    @NotNull(message = "User - Organizations are required")
+    @Size(min = 1, message = "User must belong to at least one organization")
+    private Set<UUID> organizationIds;
 
     @NotNull(message = "User - Active status is required")
     private Boolean active;
--- src/main/java/gov/cdc/izgateway/xform/services/GenericService.java
+++ src/main/java/gov/cdc/izgateway/xform/services/GenericService.java
@@ -21,6 +21,7 @@ public abstract class GenericService<T extends BaseModel> implements XformServic
     @Override
     public T getObject(UUID id) {
         T existing = repo.getEntity(id);
+
         ApiEventLogger.logReadEvent(existing);
 
         return existing;
@@ -35,7 +36,7 @@ public abstract class GenericService<T extends BaseModel> implements XformServic
 
     @Override
     public void update(T obj) {
-        T existing = repo.getEntity(obj.getId());
+        T existing = getObject(obj.getId());
 
         if (existing == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not found");
@@ -48,7 +49,7 @@ public abstract class GenericService<T extends BaseModel> implements XformServic
 
     @Override
     public void create(T obj) {
-        T existing = repo.getEntity(obj.getId());
+        T existing = getObject(obj.getId());
 
         if (existing != null) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Item already exists");
