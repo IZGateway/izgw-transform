761295eb12f8aca694e5e728e29537709a26f2ae
Austin Moody <austinmoody@protonmail.com>
2025-05-22 13:20:25 -0400

    Initial starting to add DynamoDB stuff with Organizations.

---
+++ src/main/java/gov/cdc/izgateway/xform/repository/GenericDynamoDBRepository.java
@@ -0,0 +1,84 @@
+package gov.cdc.izgateway.xform.repository;
+
+import gov.cdc.izgateway.xform.model.BaseModel;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public abstract class GenericDynamoDBRepository<T extends BaseModel> implements XformRepository<T> {
+
+    protected final DynamoDbEnhancedClient dynamoDbClient;
+    protected final String tableName;
+    protected final Class<T> entityClass;
+    protected final TableSchema<T> tableSchema;
+    protected DynamoDbTable<T> table;
+
+    protected GenericDynamoDBRepository(DynamoDbEnhancedClient dynamoDbClient, String tableName, Class<T> entityClass, TableSchema<T> tableSchema) {
+        this.dynamoDbClient = dynamoDbClient;
+        this.tableName = tableName;
+        this.entityClass = entityClass;
+        this.tableSchema = tableSchema;
+        this.table = dynamoDbClient.table(tableName, tableSchema);
+    }
+
+    @Override
+    public T getEntity(UUID id) {
+        try {
+            Key key = Key.builder().partitionValue(id.toString()).build();
+            return table.getItem(key);
+        } catch (DynamoDbException e) {
+            throw new RepositoryRuntimeException(String.format("Error retrieving entity with ID %s: %s", id, e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public Set<T> getEntitySet() {
+        try {
+            ScanEnhancedRequest scanRequest = ScanEnhancedRequest.builder().build();
+            PageIterable<T> results = table.scan(scanRequest);
+            
+            // Convert the results to a LinkedHashSet
+            return results.items().stream()
+                    .collect(Collectors.toCollection(LinkedHashSet::new));
+        } catch (DynamoDbException e) {
+            throw new RepositoryRuntimeException(String.format("Error retrieving entities: %s", e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public void createEntity(T obj) {
+        try {
+            table.putItem(obj);
+        } catch (DynamoDbException e) {
+            throw new RepositoryRuntimeException(String.format("Error creating entity: %s", e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public void updateEntity(T obj) {
+        try {
+            table.updateItem(obj);
+        } catch (DynamoDbException e) {
+            throw new RepositoryRuntimeException(String.format("Error updating entity with ID %s: %s", obj.getId(), e.getMessage()), e);
+        }
+    }
+
+    @Override
+    public void deleteEntity(UUID id) {
+        try {
+            Key key = Key.builder().partitionValue(id.toString()).build();
+            table.deleteItem(key);
+        } catch (DynamoDbException e) {
+            throw new RepositoryRuntimeException(String.format("Error deleting entity with ID %s: %s", id, e.getMessage()), e);
+        }
+    }
+}
