2a5c0c13e6eab6675a97555e58a0a722f3bbd3d3
Austin Moody <austinmoody@protonmail.com>
2025-05-23 15:48:57 -0400

    Added DynamoDb repo for OperationPreconditionField.

--- src/main/java/gov/cdc/izgateway/xform/repository/file/OperationPreconditionFieldRepository.java
+++ src/main/java/gov/cdc/izgateway/xform/repository/file/OperationPreconditionFieldRepository.java
@@ -3,10 +3,12 @@ package gov.cdc.izgateway.xform.repository.file;
 import com.fasterxml.jackson.core.type.TypeReference;
 import gov.cdc.izgateway.xform.model.OperationPreconditionField;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 
 import java.util.LinkedHashSet;
 
+@ConditionalOnProperty(name = "xform.repository.type", havingValue = "file", matchIfMissing = true)
 @Repository
 public class OperationPreconditionFieldRepository extends GenericFileRepository<OperationPreconditionField> {
 
