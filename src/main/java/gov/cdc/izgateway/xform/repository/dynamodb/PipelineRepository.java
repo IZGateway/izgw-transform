3938fa00e1c5180902ba43c9339a52a4b36d0fe7
Austin Moody <austinmoody@protonmail.com>
2025-05-24 09:13:17 -0400

    Update application.yml to spell out defaults for repository type and dynamodb table. Remove manual table schema setup for Pipeline and Solution repository as that is now handled in models.

--- src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/PipelineRepository.java
+++ src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/PipelineRepository.java
@@ -19,50 +19,11 @@ public class PipelineRepository extends GenericDynamoDBRepository<Pipeline> {
     public PipelineRepository(
             DynamoDbEnhancedClient dynamoDbClient,
             @Value("${xform.repository.dynamodb.table}") String tableName) {
-        super(dynamoDbClient, tableName, Pipeline.class, getTableSchema());
+        super(dynamoDbClient, tableName, Pipeline.class, TableSchema.fromBean(Pipeline.class));
     }
 
     @Override
     protected String getEntityName() {
         return Pipeline.class.getSimpleName();
-    }
-
-    // TODO - look at getting rid of this somehow.
-    // Can't use TableSchema.fromBean(Pipeline.class), similar to Organization
-    // because Pipelines are made up of Pipes which are then made up of
-    // preconditions
-    private static TableSchema<Pipeline> getTableSchema() {
-        return StaticTableSchema.builder(Pipeline.class)
-                .newItemSupplier(Pipeline::new)
-                .addAttribute(String.class, a -> a.name(BaseModel.ENTITY_TYPE)
-                        .getter(pipeline -> "Pipeline")
-                        .setter((pipeline, val) -> {/* Read-only attribute */})
-                        .tags(StaticAttributeTags.primaryPartitionKey()))
-                .addAttribute(String.class, a -> a.name(BaseModel.SORT_KEY)
-                        .getter(pipeline -> pipeline.getId() != null ? pipeline.getId().toString() : null)
-                        .setter((pipeline, val) -> pipeline.setId(val != null ? UUID.fromString(val) : null))
-                        .tags(StaticAttributeTags.primarySortKey()))
-                .addAttribute(String.class, a -> a.name("pipelineName")
-                        .getter(Pipeline::getPipelineName)
-                        .setter(Pipeline::setPipelineName))
-                .addAttribute(String.class, a -> a.name("organizationId")
-                        .getter(pipeline -> pipeline.getOrganizationId() != null ? pipeline.getOrganizationId().toString() : null)
-                        .setter((pipeline, val) -> pipeline.setOrganizationId(val != null ? UUID.fromString(val) : null)))
-                .addAttribute(String.class, a -> a.name("description")
-                        .getter(Pipeline::getDescription)
-                        .setter(Pipeline::setDescription))
-                .addAttribute(String.class, a -> a.name("inboundEndpoint")
-                        .getter(Pipeline::getInboundEndpoint)
-                        .setter(Pipeline::setInboundEndpoint))
-                .addAttribute(String.class, a -> a.name("outboundEndpoint")
-                        .getter(Pipeline::getOutboundEndpoint)
-                        .setter(Pipeline::setOutboundEndpoint))
-                .addAttribute(Boolean.class, a -> a.name("active")
-                        .getter(Pipeline::getActive)
-                        .setter(Pipeline::setActive))
-                .addAttribute(String.class, a -> a.name("pipes")
-                        .getter(Pipeline::getPipesJson)
-                        .setter(Pipeline::setPipesJson))
-                .build();
     }
 }
--- src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/SolutionRepository.java
+++ src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/SolutionRepository.java
@@ -19,7 +19,7 @@ public class SolutionRepository extends GenericDynamoDBRepository<Solution> {
     public SolutionRepository(
             DynamoDbEnhancedClient dynamoDbClient,
             @Value("${xform.repository.dynamodb.table}") String tableName) {
-        super(dynamoDbClient, tableName, Solution.class, getTableSchema());
+        super(dynamoDbClient, tableName, Solution.class, TableSchema.fromClass(Solution.class));
     }
 
     @Override
@@ -27,39 +27,4 @@ public class SolutionRepository extends GenericDynamoDBRepository<Solution> {
         return Solution.class.getSimpleName();
     }
 
-    // TODO - look at getting rid of this somehow.
-    // Can't use TableSchema.fromBean(Solution.class), similar to Pipeline
-    // because Solutions are made up of SolutionOperations which are then made up of
-    // complex nested objects
-    private static TableSchema<Solution> getTableSchema() {
-        return StaticTableSchema.builder(Solution.class)
-                .newItemSupplier(Solution::new)
-                .addAttribute(String.class, a -> a.name(BaseModel.ENTITY_TYPE)
-                        .getter(solution -> "Solution")
-                        .setter((solution, val) -> {/* Read-only attribute */})
-                        .tags(StaticAttributeTags.primaryPartitionKey()))
-                .addAttribute(String.class, a -> a.name(BaseModel.SORT_KEY)
-                        .getter(solution -> solution.getId() != null ? solution.getId().toString() : null)
-                        .setter((solution, val) -> solution.setId(val != null ? UUID.fromString(val) : null))
-                        .tags(StaticAttributeTags.primarySortKey()))
-                .addAttribute(String.class, a -> a.name("solutionName")
-                        .getter(Solution::getSolutionName)
-                        .setter(Solution::setSolutionName))
-                .addAttribute(String.class, a -> a.name("description")
-                        .getter(Solution::getDescription)
-                        .setter(Solution::setDescription))
-                .addAttribute(String.class, a -> a.name("version")
-                        .getter(Solution::getVersion)
-                        .setter(Solution::setVersion))
-                .addAttribute(Boolean.class, a -> a.name("active")
-                        .getter(Solution::getActive)
-                        .setter(Solution::setActive))
-                .addAttribute(String.class, a -> a.name("requestOperations")
-                        .getter(Solution::getRequestOperationsJson)
-                        .setter(Solution::setRequestOperationsJson))
-                .addAttribute(String.class, a -> a.name("responseOperations")
-                        .getter(Solution::getResponseOperationsJson)
-                        .setter(Solution::setResponseOperationsJson))
-                .build();
-    }
 }
