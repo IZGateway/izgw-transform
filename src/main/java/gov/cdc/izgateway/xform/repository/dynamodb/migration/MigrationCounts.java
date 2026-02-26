4c95ff06fe16aa74cf26197c85d73c772f8c6f83
ebrockainq <112428189+ebrockainq@users.noreply.github.com>
2026-02-26 09:19:31 -0500

    Address Javadoc failures
    Addressed missing comments

--- src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/migration/MigrationCounts.java
+++ src/main/java/gov/cdc/izgateway/xform/repository/dynamodb/migration/MigrationCounts.java
@@ -1,7 +1,11 @@
 package gov.cdc.izgateway.xform.repository.dynamodb.migration;
 
 /**
- * Just here to track migration counts.
+ * Tracks migration counts.
+ *
+ * @param total Total number of entities evaluated.
+ * @param migrated Number of entities migrated.
+ * @param skipped Number of entities skipped.
  */
 public record MigrationCounts(int total, int migrated, int skipped) {
 }
